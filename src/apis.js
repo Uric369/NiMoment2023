e05185b08a393db32e5914f19d380ffa4bec3b2a
YBRua <ybiur0929@163.com>
2023-11-28 15:33:51 +0800

    Added nimoment apis

--- src/apis.js
+++ src/apis.js
@@ -1,3 +1,38 @@
+const nimomentUrlBase = "https://nimo.sjtu.edu.cn/nimoment/api";
+export const nimoerApi = `${nimomentUrlBase}/nimoer`;
+export const departmentStatsApi = `${nimomentUrlBase}/department`;
+export const personalStatsGeneralApi = `${nimomentUrlBase}/stats_general`;
+export const personalStatsOfficeApi = `${nimomentUrlBase}/stats_office`;
+export const personalStatsProgressApi = `${nimomentUrlBase}/stats_progress`;
+export const personalStatsFieldApi = `${nimomentUrlBase}/stats_field`;
+
+function getDefaultHeaders() {
+  return {
+    "Content-Type": "application/json",
+  };
+}
+
+export function getRequest(url, onSuccess, onError) {
+  let opts = {
+    method: "GET",
+    headers: getDefaultHeaders(),
+    credentials: "include",
+  };
+
+  fetch(url, opts)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      onSuccess(data);
+    })
+    .catch((error) => {
+      if (onError) {
+        onError(error);
+      }
+    });
+}
+
 export async function getCurrentUser() {
   const res = await fetch("/kaleid/api/curuser");
   return await res.json();
