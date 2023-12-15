647bc64010a9edb83d13a29bd9df8bf15d5622be
YBRua <ybiur0929@163.com>
2023-12-15 13:43:23 +0800

    Format

--- src/RetireView/Personal2.js
+++ src/RetireView/Personal2.js
@@ -38,7 +38,6 @@ import {
   getRequest,
 } from "../apis";
 
-
 const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
 function formatProfileInfo(profile) {
@@ -120,166 +119,182 @@ const Personal2Special = () => {
 
   return (
     <div>
-    <div
-      className="personal2special_container"
-      style={{
-        backgroundImage: `url(${BG1})`,
-        backgroundSize: "cover",
-        width: "100vw",
-        height: "100vh",
-      }}
-      ref={containerRef}
-    >
-      <div className="title_container">
-        <div className="header">
-          <img
-            style={{ width: isMobile ? "12vw" : "6vw" }}
-            src={logo}
-            alt="logo"
-          />
-          <h1>NIMO 来信 · 职业生涯回顾</h1>
-        </div>
-      </div>
-
-      <div className="outer_container">
-        <div className="inner_container" style={{ display: "flex" }}>
-          <img
-            style={{
-              width: isMobile ? "12vh" : "25vh",
-              height: isMobile ? "13vh" : "27vh",
-              marginRight: "2vw",
-            }}
-            src={profileImg}
-          />
-          <div>
-            <p>&gt;&gt;&gt; </p>
-            {formatProfileInfo(profileInfo).map((info, index) => (
-              <Combination4
-                key={index}
-                item={info.item}
-                content={info.content}
-              />
-            ))}
+      <div
+        className="personal2special_container"
+        style={{
+          backgroundImage: `url(${BG1})`,
+          backgroundSize: "cover",
+          width: "100vw",
+          height: "100vh",
+        }}
+        ref={containerRef}
+      >
+        <div className="title_container">
+          <div className="header">
+            <img
+              style={{ width: isMobile ? "12vw" : "6vw" }}
+              src={logo}
+              alt="logo"
+            />
+            <h1>NIMO 来信 · 职业生涯回顾</h1>
           </div>
-          {!isMobile && (
-            <div style={{ marginLeft: "10vw" }}>
-              <IconTitle icon={titleIcon} text={"累计霍霍器材"} />
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
-                {formatPersonalConsumables(consumables)
-                  .slice(0, 2)
-                  .map((data, index) => (
-                    <div style={{ marginRight: "2vw" }}>
-                      <IconCount
-                        key={index}
-                        icon={data.icon}
-                        count={data.count}
-                        height={4}
-                      />
-                    </div>
+        </div>
+
+        <div className="outer_container">
+          <div className="inner_container" style={{ display: "flex" }}>
+            <img
+              style={{
+                width: isMobile ? "12vh" : "25vh",
+                height: isMobile ? "13vh" : "27vh",
+                marginRight: "2vw",
+              }}
+              src={profileImg}
+            />
+            <div>
+              <p>&gt;&gt;&gt; </p>
+              {formatProfileInfo(profileInfo).map((info, index) => (
+                <Combination4
+                  key={index}
+                  item={info.item}
+                  content={info.content}
+                />
+              ))}
+            </div>
+            {!isMobile && (
+              <div style={{ marginLeft: "10vw" }}>
+                <IconTitle icon={titleIcon} text={"累计霍霍器材"} />
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  {formatPersonalConsumables(consumables)
+                    .slice(0, 2)
+                    .map((data, index) => (
+                      <div style={{ marginRight: "2vw" }}>
+                        <IconCount
+                          key={index}
+                          icon={data.icon}
+                          count={data.count}
+                          height={4}
+                        />
+                      </div>
+                    ))}
+                </div>
+                <div>
+                  <IconCount
+                    icon={formatPersonalConsumables(consumables)[2].icon}
+                    count={formatPersonalConsumables(consumables)[2].count}
+                    height={4}
+                  />
+                </div>
+              </div>
+            )}
+
+            <div className="inner_bottom_container">
+              <div>
+                <p style={{ fontSize: isMobile ? "1.5vh" : "3vh" }}>
+                  &gt;&gt;&gt; NIMO@localhost: loading log......
+                </p>
+                <div style={{ lineHeight: "1.7" }}>
+                  {formatStatistics(statistics).map((stat, index) => (
+                    <Combination5
+                      key={index}
+                      text1={stat.text1}
+                      count={stat.count}
+                      text2={stat.text2}
+                    />
                   ))}
+                </div>
               </div>
               <div>
-                <IconCount
-                  icon={formatPersonalConsumables(consumables)[2].icon}
-                  count={formatPersonalConsumables(consumables)[2].count}
-                  height={4}
-                />
-              </div>
-            </div>
-          )}
-
-          <div className="inner_bottom_container">
-            <div>
-              <p style={{ fontSize: isMobile ? "1.5vh" : "3vh" }}>
-                &gt;&gt;&gt; NIMO@localhost: loading log......
-              </p>
-              <div style={{ lineHeight: "1.7" }}>
-                {formatStatistics(statistics).map((stat, index) => (
-                  <Combination5
-                    key={index}
-                    text1={stat.text1}
-                    count={stat.count}
-                    text2={stat.text2}
+                <div
+                  style={{
+                    position: "absolute",
+                    top: isMobile ? "30vh" : "10vh",
+                    left: isMobile ? "0vw" : "30vw",
+                  }}
+                >
+                  <IconTitle
+                    icon={titleIcon}
+                    text={"报修最常去的楼栋"}
+                    isMargin={false}
                   />
-                ))}
-              </div>
-            </div>
-            <div>
-              <div
-                style={{
-                  position: "absolute",
-                  top: isMobile ? "30vh" : "10vh",
-                  left: isMobile ? "0vw" : "30vw",
-                }}
-              >
-                <IconTitle
-                  icon={titleIcon}
-                  text={"报修最常去的楼栋"}
-                  isMargin={false}
-                />
-                <div className="baoxiu_cat1">
-                <h4>
-                  {hifrequencies.building}
-                </h4>
-                </div>
-              </div>
-
-              <div
-                style={{
-                  position: "absolute",
-                  top: isMobile ? "41vh" : "32vh",
-                  left: isMobile ? "0vw" : "30vw",
-                }}
-              >
-                <IconTitle
-                  icon={titleIcon}
-                  text={"报修最常一起去的人"}
-                  isMargin={false}
-                />
-                <div className="baoxiu_cat2">
-                <h5>
-                  {hifrequencies.colleague}
-                </h5>
-                </div>
-              </div>
-              {isMobile && (
-                <div style={{ position: "absolute", top: "54vh", left: "0vw" }}>
-                  <IconTitle icon={titleIcon} text={"累计霍霍器材"} />
-                  <div
-                    style={{ display: "flex", justifyContent: "space-between",zIndex:2 }}
-                  >
-                    {formatPersonalConsumables(consumables)
-                      .map((data, index) => (
-                        <div style={{ marginRight: "2vw" }}>
-                          <IconCount
-                            key={index}
-                            icon={data.icon}
-                            count={data.count}
-                            height={10}
-                          />
-                        </div>
-                      ))}
+                  <div className="baoxiu_cat1">
+                    <h4>{hifrequencies.building}</h4>
                   </div>
-                  {/* <div>
+                </div>
+
+                <div
+                  style={{
+                    position: "absolute",
+                    top: isMobile ? "41vh" : "32vh",
+                    left: isMobile ? "0vw" : "30vw",
+                  }}
+                >
+                  <IconTitle
+                    icon={titleIcon}
+                    text={"报修最常一起去的人"}
+                    isMargin={false}
+                  />
+                  <div className="baoxiu_cat2">
+                    <h5>{hifrequencies.colleague}</h5>
+                  </div>
+                </div>
+                {isMobile && (
+                  <div
+                    style={{ position: "absolute", top: "54vh", left: "0vw" }}
+                  >
+                    <IconTitle icon={titleIcon} text={"累计霍霍器材"} />
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        zIndex: 2,
+                      }}
+                    >
+                      {formatPersonalConsumables(consumables).map(
+                        (data, index) => (
+                          <div style={{ marginRight: "2vw" }}>
+                            <IconCount
+                              key={index}
+                              icon={data.icon}
+                              count={data.count}
+                              height={10}
+                            />
+                          </div>
+                        )
+                      )}
+                    </div>
+                    {/* <div>
                     <IconCount
                       icon={formatPersonalConsumables(consumables)[2].icon}
                       count={formatPersonalConsumables(consumables)[2].count}
                       height={10}
                     />
                   </div> */}
-                </div>
-              )}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
+        <div className="cat_container" onClick={handleRouter}>
+          <ImageTransition cat1={cat1} cat2={cat2} size={isMobile ? 35 : 20} />
+        </div>
+
+        <img
+          src={next}
+          onClick={handleRouter}
+          style={{
+            position: "absolute",
+            bottom: "5vh",
+            right: "4vw",
+            width: isMobile ? "20vw" : "8vw",
+            height: "auto",
+            cursor: "pointer",
+          }}
+        />
       </div>
-      <div className="cat_container" onClick={handleRouter}>
-        <ImageTransition cat1={cat1} cat2={cat2} size={isMobile? 35:20} />
-      </div>
-      
-      <img src={next} onClick={handleRouter} style={{position:"absolute", bottom:"5vh", right:"4vw",width:isMobile? "20vw":"8vw",height:"auto",cursor:"pointer"}}/>
-    </div>
-    <img src={saveButton} onClick={onClick} className="savebutton"/>
+      <img src={saveButton} onClick={onClick} className="savebutton" />
     </div>
   );
 };
